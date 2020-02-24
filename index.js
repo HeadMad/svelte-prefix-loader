9550b38971de0751f83bd90e6d2c42ab5639db43
HeadMad <kov.ps@ya.ru>
2020-02-24 14:05:37 +0800

    new version

--- index.js
+++ index.js
@@ -1,99 +1,108 @@
 
 const HTMLParser = require('./lib/HTMLParser')
 const path = require('path')
+let firstFilePath
+const fileImports = new Set()
+const allPrefixedTags = new Map()
 
-
-const getPrefixesTags = (prefixes, stack) => async ({content}) => {
-
-  
-  let tags = []
-  
-  await HTMLParser(content, {
-    start: function ({ rawTagName }) {
-      if (/(style|script)/.test(rawTagName)) return
-      tags.push(rawTagName)
-    }
-  })
-
-  prefixes.forEach(prefix => {
-    tags = tags.filter(tag => {
-      if (!tag.startsWith(prefix)) return true
-      stack.add(tag)
-    })
+const convertPath = (string, search, repl) => {
+  let regExp = new RegExp('\\[([^\\]]*)(' + search + ')\\]', 'ig');
+  return string.replace(regExp, (_, pre, search) => {
+    // if empty repl
+    if (!repl) return ''
+    // if had no pre
+    pre = pre ? pre : ''
+    // if [Block] we use Pascal notacion
+    let isPascal = /^[A-Z]/.test(search)
+    let fill = isPascal ? repl : repl.toLowerCase()
+    return pre + fill
   })
 }
 
-const searchImports = (content, stack) => {
-  const REImports = /import\s+(\S+)\s+from\s+/g
-  let matches = content.matchAll(REImports)
-  let imports = new Set([...matches].map(match => match[1]))
-  
-  return stack.filter(each => !imports.has(each))
+const parseTagPath = (tag, prefixPath) => {
+  const splitedTag = tag.replace(/\B([A-Z])/g, '|$1').split('|')
+  let parsedPath = prefixPath;
+  ['prefix', 'block', 'elem'].forEach((search, i) => {
+    parsedPath = convertPath(parsedPath, search, splitedTag[i])
+  })
+  return path.join(firstFilePath, parsedPath).replace(/\\/g, '/') + '.svelte'
 }
 
-const createImportsArray = (map, components) => {
-
-  return components
-    .map(comp => comp.replace(/\B([A-Z])/g, '|$1').split('|'))
-    .map(([prefix, block, elem], i) => {
-      let component = components[i]
-      let importPath = map.get(component)
-      if (block)  importPath = pathReplace(importPath, 'block', block)   // replace [block] or [Block](Pascal case)
-      if (elem)   importPath = pathReplace(importPath, 'elem', elem)     // replace [elem] or [Elem](Pascal case)
-      if (prefix) importPath = pathReplace(importPath, 'prefix', prefix) // replace [prefix] or [Prefix](Pascal case)
-      return `import ${component} from "${importPath}.svelte"`
+/**
+ * 
+ * @param {*} tags 
+ * @param {*} prefixes 
+ * @return {Map} [[tag, fullPath], ...]
+ */
+const sortTagsList = (tags, prefixes) => {
+  const result = new Map();
+  tags.forEach(tag => {
+    prefixes.forEach(([prefix, prefixPath]) => {
+      if (!tag.startsWith(prefix) || result.has(tag)) return
+      if (allPrefixedTags.has(tag)) {
+        result.set(tag, allPrefixedTags.get(tag))
+      } else {
+        let parsedPath = parseTagPath(tag, prefixPath)
+        allPrefixedTags.set(tag, parsedPath)
+        result.set(tag, parsedPath)
+      }
     })
-
+  })
+  return result
 }
 
-const insertImportsOfComponents = (map, stack) => ({content}) => {
-
-  let lostImports = searchImports(content, [...stack])
-  let importsArray = createImportsArray(map, lostImports)
-
-  let code = `\n  ${importsArray.join('\n  ')}\n${content}`
-  console.log(code)
-  return {code}
-}
-
-
-const prefixLoader = opts => {
-  
-  if (!opts) return {}
-
-  let map = new Map(Object.entries(opts))
-  let stack = new Set()
-
-  let markup = getPrefixesTags([...map.keys()], stack)
-  let script = insertImportsOfComponents(map, stack)
-
-  return { markup, script }
-}
-
-module.exports = prefixLoader
 
 
 /**
  * 
- * @param {String} string String fore replace serch substring
- * @param {String} search Search substring
- * @param {String} repl Replacement string
+ * @param {*} content 
+ * @param {*} prefixes 
+ * @retun {Map} as [[tag, fullPath]]
  */
-function pathReplace(string, search, repl)
-{
-  // if empty repl
-  if (!repl) return ''
-
-  let regExp = new RegExp('\\[([^\\]]*)(' + search + ')\\]', 'ig');
-  return string.replace(regExp, (_, pre, search) => {
-    
-    // if had no pre
-    pre = pre ? pre : ''
-    
-    // if [Block] we use Pascal notacion
-    let isPascal = /^[A-Z]/.test(search)
-    let fill = isPascal ? repl : repl.toLowerCase()
-
-    return pre + fill
+const getTagsList = (content, prefixes) => {
+  const tags = new Set()
+  HTMLParser(content, {
+    start({rawTagName: tag}) {
+      if (/(script|style)/.test(tag)) return
+      tags.add(tag)
+    }
   })
-} 
+  return sortTagsList(tags, prefixes)
+}
+
+const addFileImports = (content, prefixes) => {
+  fileImports.clear()
+  const tagsList = getTagsList(content, prefixes)
+  tagsList.forEach((fullPath, tag) => {
+    fileImports.add(`import ${tag} from "${fullPath}"`)
+  })
+}
+
+const findPrefixTags = (prefixes) => ({content, filename}) => {
+  const filePath = path.dirname(filename)
+  firstFilePath = firstFilePath || filePath
+  addFileImports(content, prefixes)
+}
+
+const insertLostImports = ({content}) => {
+  const code = `\n ${[...fileImports].join('\n')} \n ${content}`
+  return {code}
+}
+
+const prefixLoader = (opts) => {
+  if (!opts) return {}
+  const prefixes = Object.entries(opts)
+  const markup = findPrefixTags(prefixes)
+  const script = insertLostImports
+  return {markup, script}
+}
+
+const trowing = (handler) => (...args) => {
+  try {
+    return handler(...args)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = prefixLoader
